c545629350d03c36b1eafee9917ffb8baa0a7e24
Fernando Herrera <fernando.herrera85@gmail.com>
2024-09-04 10:54:50 -0400

    chore: Update dependencies and remove unused code

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  // NOTE: Update this to include the paths to all of your component files.
+  content: [
+    "./app/**/*.{js,jsx,ts,tsx}",
+    "./presentation/**/*.{js,jsx,ts,tsx}",
+  ],
+  presets: [ require( "nativewind/preset" ) ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
