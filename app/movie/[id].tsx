93614e03ccd444adcb4f6314cf9e5cc125484d14
Fernando Herrera <fernando.herrera85@gmail.com>
2024-09-05 14:46:29 -0400

    feat: Add MovieHeader component for displaying movie details

--- app/movie/[id].tsx
+++ app/movie/[id].tsx
@@ -1,3 +1,4 @@
+import MovieHeader from '@/presentation/components/movie/MovieHeader';
 import { useMovie } from '@/presentation/hooks/useMovie';
 import { useLocalSearchParams } from 'expo-router';
 import { View, Text, ActivityIndicator, ScrollView } from 'react-native';
@@ -6,7 +7,7 @@ const MovieScreen = () => {
 
   const { movieQuery } = useMovie(+id);
 
-  if (movieQuery.isLoading) {
+  if (movieQuery.isLoading || !movieQuery.data) {
     return (
       <View className="flex flex-1 justify-center items-center">
         <Text className="mb-4">Espere por favor</Text>
@@ -17,7 +18,11 @@ const MovieScreen = () => {
 
   return (
     <ScrollView>
-      <Text>{movieQuery.data?.title ?? 'No tiene'}</Text>
+      <MovieHeader
+        originalTitle={movieQuery.data.originalTitle}
+        poster={movieQuery.data.poster}
+        title={movieQuery.data.title}
+      />
     </ScrollView>
   );
 };
---
+++ presentation/components/movie/MovieHeader.tsx
@@ -0,0 +1,61 @@
+import {
+  View,
+  Text,
+  useWindowDimensions,
+  Image,
+  Pressable,
+} from 'react-native';
+import { Ionicons } from '@expo/vector-icons';
+import { router } from 'expo-router';
+
+interface Props {
+  poster: string;
+  originalTitle: string;
+  title: string;
+}
+
+const MovieHeader = ({ poster, originalTitle, title }: Props) => {
+  const { height: screenHeight } = useWindowDimensions();
+
+  return (
+    <>
+      <View
+        style={{
+          position: 'absolute',
+          zIndex: 99,
+          elevation: 9,
+          top: 40,
+          left: 10,
+        }}
+      >
+        <Pressable onPress={() => router.dismiss()}>
+          <Ionicons
+            name="arrow-back"
+            size={30}
+            color="white"
+            className="shadow"
+          />
+        </Pressable>
+      </View>
+
+      <View
+        style={{ height: screenHeight * 0.7 }}
+        className="shadow-xl shadow-black/20"
+      >
+        <View className="flex-1 rounded-b-[25px] overflow-hidden">
+          <Image
+            source={{ uri: poster }}
+            resizeMode="cover"
+            className="flex-1"
+          />
+        </View>
+      </View>
+
+      <View className="px-5 mt-5">
+        <Text className="font-normal">{originalTitle}</Text>
+        <Text className="font-semibold text-2xl">{title}</Text>
+      </View>
+    </>
+  );
+};
+export default MovieHeader;
